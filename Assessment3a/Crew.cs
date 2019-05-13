013d806e53708ad6db4d2a01312021722ae93fdb
hmcenhill <47829754+hmcenhill@users.noreply.github.com>
2019-05-13 19:33:07 -0400

    Add files via upload

---
+++ Assessment3a/Crew.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment3a
+{
+    public class Crew
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public Crew(string name, string role)
+        {
+            Name = name;
+            Role = role;
+        }
+    }
+}
---
+++ Assessment3a/SpaceShuttle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment3a
+{
+    public class SpaceShuttle
+    {
+        public int Fuel { get; set; }
+        public List<Crew> Team { get; set; }
+        public SpaceShuttle()
+        {
+            Fuel = 0;
+            Team = new List<Crew>();
+            Team.Add(new Crew("John", "Captain"));
+            Team.Add(new Crew("Diddy", "Chimp"));
+            Team.Add(new Crew("Hannah", "Navigator"));
+        }
+        public bool Launch()
+        {
+            return Fuel > 5 && Team.Count == 3;
+        }
+    }
+}
